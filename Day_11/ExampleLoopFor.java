d0a8f79fcf24c03b4ad1c13089b8c1babf1ced11
Evelin <80602149+Evelin-76@users.noreply.github.com>
2021-04-04 19:56:06 +0200

    Add files via upload

---
+++ Day_11/ExampleLoopFor.java
@@ -0,0 +1,24 @@
+package Day_11;
+
+public class ExampleLoopFor {
+
+	public static void main(String[] args) {
+		
+		int x=0;
+		int y=0;
+		for( y=0; x<5 && y<10; x++, y++) {
+			
+			System.out.print(y + " ");
+			
+			System.out.print(x + " ");
+			
+		}
+		System.out.println();
+		
+		System.out.println(x+y);
+
+	}
+	
+	
+
+}
